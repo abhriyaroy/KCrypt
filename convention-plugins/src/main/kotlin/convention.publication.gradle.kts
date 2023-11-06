5db656642b1fa6259ab2cd1738664aa6ac2dbd24
Abhriya <abhriya.roy@biofourmis.com>
2023-11-06 20:42:35 +0530

    Release v 0.0.9

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -46,7 +46,7 @@ publishing {
     maven {
       name = "KCrypt"
       group = "io.github.abhriyaroy"
-      version = "0.0.8"
+      version = "0.0.9"
       setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
       credentials {
         username = getExtraString("ossrhUsername")
